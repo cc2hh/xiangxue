4b28051a1d64973e60e6b06da498e8a75d963ad5
cc2hh <lccovhhe>
2020-11-25 14:04:53 +0800

    version.gradle配置

--- app/src/main/java/com/cc/test/module/MainActivity.java
+++ app/src/main/java/com/cc/test/module/MainActivity.java
@@ -3,13 +3,20 @@ package com.cc.test.module;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+    }
+
+    private void init() {
+        Log.d(TAG, "url:" + BuildConfig.url);
     }
 }
